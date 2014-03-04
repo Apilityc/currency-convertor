7b8237e38bd90f91e6ea9aa961c75842869c2205
gogoluxecs <georgi.lambov@gmail.com>
2014-03-04 22:27:16 +0200

    Demo with redis integration test.

--- src/test/java/org/apilytc/domain/CurrencyIntegrationTest.java
+++ src/test/java/org/apilytc/domain/CurrencyIntegrationTest.java
@@ -1,11 +1,35 @@
 package org.apilytc.domain;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
 @RooIntegrationTest(entity = Currency.class, transactional = false)
 public class CurrencyIntegrationTest {
 
-    @Test
-    public void testMarkerMethod() {
-    }
+	@Autowired
+	private RedisTemplate<String, String> template;
+
+	@Resource(name = "redisTemplate")
+	private ListOperations<String, String> listOpts;
+
+	@Resource(name = "redisTemplate")
+	private ValueOperations<String, String> valueOpts;
+
+	@Test
+	public void testMarkerMethod() {
+	}
+
+	@Test
+	public void testRedisAddLink() {
+
+		listOpts.leftPush("left", "right");
+		valueOpts.set("sah", "bah");
+
+	}
 }
