e6251a51e487fc3426f7ac32546d68634b36e63c
gogoluxecs <georgi.lambov@gmail.com>
2014-03-11 05:14:33 +0200

    Initial structure for ingestion of rates and currency.

---
+++ src/main/java/org/apilytc/currency/ingestion/rate/RateIngestion.java
@@ -0,0 +1,6 @@
+package org.apilytc.currency.ingestion.rate;
+
+public interface RateIngestion {
+
+	public void execute();
+}
---
+++ src/main/java/org/apilytc/currency/ingestion/vapor/VaporIngestion.java
@@ -0,0 +1,6 @@
+package org.apilytc.currency.ingestion.vapor;
+
+public interface VaporIngestion {
+
+	public void execute();
+}
