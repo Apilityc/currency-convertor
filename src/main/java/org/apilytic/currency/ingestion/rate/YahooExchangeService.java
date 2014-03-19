34bfa9e01b172b104f136deff4beefb69e19b9c7
gogoluxecs <georgi.lambov@gmail.com>
2014-03-19 23:53:11 +0200

    Remove warning.

--- src/main/java/org/apilytic/currency/ingestion/rate/YahooExchangeService.java
+++ src/main/java/org/apilytic/currency/ingestion/rate/YahooExchangeService.java
@@ -30,5 +30,7 @@ public class YahooExchangeService implements RateIngestion {
 		List<? extends ExchangeRate> providedRates = financialProvider
 				.provideRate();
 
+		providedRates.size();
+
 	}
 }
