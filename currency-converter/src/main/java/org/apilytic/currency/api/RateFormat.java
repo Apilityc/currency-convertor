f73ed5810a9fbb8034be742519da489f67ba269d
GoGoLuXecS <georgi.lambov@gmail.com>
2014-08-10 00:29:31 +0300

    Verification of Locale for currency formatting.

--- currency-converter/src/main/java/org/apilytic/currency/api/RateFormat.java
+++ currency-converter/src/main/java/org/apilytic/currency/api/RateFormat.java
@@ -2,6 +2,8 @@ package org.apilytic.currency.api;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
+
 /**
  * Created by g on 7/24/14.
  */
@@ -15,13 +17,28 @@ public class RateFormat {
      * @return
      */
     public String cleanNumber(String number) {
-    	String charsAndWhiteSpaces = "[^\\d\\.,]|[\\s*-]";
-    	String commas = ",+?";
-    	
-    	if(number.matches(".*\\.\\d+$")) {
-    		return number.replaceAll(charsAndWhiteSpaces, "").replaceAll(commas, "");
-    	}
-    	
+        String charsAndWhiteSpaces = "[^\\d\\.,]|[\\s*-]";
+        String commas = ",+?";
+
+        if (number.matches(".*\\.\\d+$")) {
+            return number.replaceAll(charsAndWhiteSpaces, "").replaceAll(commas, "");
+        }
+
         return number.replaceAll(charsAndWhiteSpaces, "").replaceAll(",(?!.*,)", ".").replaceAll(commas, "");
+    }
+
+    /**
+     * Verifies if passed language is valid and format currency according to it afterwards.
+     *
+     * @return
+     */
+    public Locale verifyLocale(String language) {
+
+        for (String locale : Locale.getISOLanguages()) {
+            if (language.equals(locale)) {
+                return new Locale(language);
+            }
+        }
+        return null;
     }
 }
--- currency-converter/src/test/java/org/apilytic/currency/api/RateFormatTest.java
+++ currency-converter/src/test/java/org/apilytic/currency/api/RateFormatTest.java
@@ -1,73 +1,73 @@
 package org.apilytic.currency.api;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import java.util.Locale;
-
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Locale;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
 public class RateFormatTest {
 
-	private RateFormat convertRateFormat;
+    private RateFormat convertRateFormat;
 
-	@BeforeClass
-	public void init() {
-		convertRateFormat = new RateFormat();
-	}
+    @BeforeClass
+    public void init() {
+        convertRateFormat = new RateFormat();
+    }
 
-	@Test
-	public void cleanNumberOfInteger() {
-		assertThat(convertRateFormat.cleanNumber("100"), equalTo("100"));
-	}
+    @Test
+    public void cleanNumberOfInteger() {
+        assertThat(convertRateFormat.cleanNumber("100"), equalTo("100"));
+    }
 
-	@Test
-	public void cleanNumberOfDecimal() {
-		assertThat(convertRateFormat.cleanNumber("100.12"), equalTo("100.12"));
-	}
+    @Test
+    public void cleanNumberOfDecimal() {
+        assertThat(convertRateFormat.cleanNumber("100.12"), equalTo("100.12"));
+    }
 
-	@Test
-	public void cleanNumberOfDecimalUs() {
-		java.text.NumberFormat format = java.text.NumberFormat
-				.getCurrencyInstance(new Locale("en", "US"));
-		assertThat(convertRateFormat.cleanNumber(format.format(100.12)),
-				equalTo("100.12"));
-		assertThat(convertRateFormat.cleanNumber(format.format(100010100.12)),
-				equalTo("100010100.12"));
-	}
+    @Test
+    public void cleanNumberOfDecimalUs() {
+        java.text.NumberFormat format = java.text.NumberFormat
+                .getCurrencyInstance(new Locale("en", "US"));
+        assertThat(convertRateFormat.cleanNumber(format.format(100.12)),
+                equalTo("100.12"));
+        assertThat(convertRateFormat.cleanNumber(format.format(100010100.12)),
+                equalTo("100010100.12"));
+    }
 
-	@Test
-	public void cleanNumberOfDecimalEuro() {
-		assertThat(convertRateFormat.cleanNumber("100.12EUR"),
-				equalTo("100.12"));
-		assertThat(convertRateFormat.cleanNumber("100.12 EUR"),
-				equalTo("100.12"));
-	}
+    @Test
+    public void cleanNumberOfDecimalEuro() {
+        assertThat(convertRateFormat.cleanNumber("100.12EUR"),
+                equalTo("100.12"));
+        assertThat(convertRateFormat.cleanNumber("100.12 EUR"),
+                equalTo("100.12"));
+    }
 
-	@Test
-	public void cleanNumberOfCommaSeparatedDecimals() {
-		assertThat(convertRateFormat.cleanNumber("100,12"), equalTo("100.12"));
-		assertThat(convertRateFormat.cleanNumber("1,00,12"), equalTo("100.12"));
-	}
+    @Test
+    public void cleanNumberOfCommaSeparatedDecimals() {
+        assertThat(convertRateFormat.cleanNumber("100,12"), equalTo("100.12"));
+        assertThat(convertRateFormat.cleanNumber("1,00,12"), equalTo("100.12"));
+    }
 
-	@Test
-	public void cleanNumberOfLargeInteger() {
-		assertThat(convertRateFormat.cleanNumber("1001210012"),
-				equalTo("1001210012"));
-		assertThat(convertRateFormat.cleanNumber("10012 100 12"),
-				equalTo("1001210012"));
-	}
+    @Test
+    public void cleanNumberOfLargeInteger() {
+        assertThat(convertRateFormat.cleanNumber("1001210012"),
+                equalTo("1001210012"));
+        assertThat(convertRateFormat.cleanNumber("10012 100 12"),
+                equalTo("1001210012"));
+    }
 
-	@Test
-	public void notValidLocalFromString() {
-		String inputCountry = "not-valid-language";
+    @Test
+    public void verifiedLocale() {
+        assertNotNull(convertRateFormat.verifyLocale("en"));
+    }
 
-		for (String locale : Locale.getISOLanguages()) {
-			if (inputCountry.equals(locale)) {
-				Assert.fail();
-			}
-		}
-	}
+    @Test
+    public void notVerifiedLocale() {
+        assertNull(convertRateFormat.verifyLocale("us"));
+    }
 }
