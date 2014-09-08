4318d9dd5d172b644addfd874a7073d935e2b949
gogoluxecs <georgi.lambov@gmail.com>
2014-09-08 09:51:57 +0300

    DirectDeal exchange api integration tests.

--- currency-converter/src/test/java/org/apilytic/currency/api/DirectDealExchangeApiIT.java
+++ currency-converter/src/test/java/org/apilytic/currency/api/DirectDealExchangeApiIT.java
@@ -3,6 +3,8 @@ package org.apilytic.currency.api;
 import static org.testng.AssertJUnit.assertTrue;
 import static org.testng.AssertJUnit.assertNotNull;
 
+import org.apilytic.currency.api.model.CurrencyRate;
+import org.apilytic.currency.api.model.ExchangeCurrency;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
@@ -28,6 +30,33 @@ public class DirectDealExchangeApiIT {
 	public void createDefaultInstance() {
 		assertNotNull(defaultCurrencyExchangeApi);
 		assertTrue(defaultCurrencyExchangeApi instanceof DirectDealExchangeApi);
+	}
+	
+	@Test
+	public void calculateExchange() {
+		CurrencyRate currencyRate = new CurrencyRate();
+		currencyRate.setAmount("5.45");
+		currencyRate.setFromCurrency("USD");
+		currencyRate.setToCurrency("EUR");
+		
+		ExchangeCurrency exchange = directDealExchangeApi.exchangeSingleCurrency(currencyRate);
+		assertNotNull(exchange.getExchange());
+		assertTrue(exchange.getExchange().equals(0) == false);
+		assertTrue(Double.valueOf(exchange.getExchange()) > 0);
+	}
+	
+	@Test
+	public void calculateExchangeWithLocaleFormat() {
+		CurrencyRate currencyRate = new CurrencyRate();
+		currencyRate.setAmount("5.45");
+		currencyRate.setFromCurrency("USD");
+		currencyRate.setToCurrency("EUR");
+		currencyRate.setLocale("en_US");
+		
+		ExchangeCurrency exchange = directDealExchangeApi.exchangeSingleCurrency(currencyRate);
+		assertTrue(exchange.getExchange().startsWith("$"));
+		assertTrue(Double.valueOf(exchange.getExchange().replace("$","")) > 0);
+		
 	}
 	
 }
