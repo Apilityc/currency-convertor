86164f31dc9ce82e2924531237082023c31f174b
gogoluxecs <georgi.lambov@gmail.com>
2014-10-09 16:39:31 +0300

    Adding hashmap init with lambda.

--- currency-converter/src/test/java/org/apilytic/currency/api/KeyStoreExchangeApiIT.java
+++ currency-converter/src/test/java/org/apilytic/currency/api/KeyStoreExchangeApiIT.java
@@ -4,7 +4,6 @@ import static org.testng.AssertJUnit.assertNotNull;
 import static org.testng.AssertJUnit.assertTrue;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import org.apilytic.currency.api.model.CurrencyRate;
 import org.apilytic.currency.api.model.ExchangeCurrency;
@@ -23,54 +22,57 @@ import org.testng.annotations.Test;
 public class KeyStoreExchangeApiIT {
 
 	private static final String EXCHANGE_API = "keyStoreExchangeApi";
-	
+
 	@Autowired
 	@Qualifier(EXCHANGE_API)
 	private CurrencyExchangeApi keyStoreExchangeApi;
-	
+
 	@Autowired
 	private RateRepository rateRepository;
-	
+
 	@Autowired
 	@Qualifier(EXCHANGE_API)
 	private CurrencyExchangeApi optionalCurrencyExchangeApi;
 
 	@BeforeClass
 	public void init() {
-		Map<String, String> value = new HashMap<String, String>();
-		value.put("GBP", "5.45");
-		
 		Rate rate = new Rate();
 		rate.setKey(Rate.key("EUR"));
-		rate.setValue(value);
+		rate.setValue(new HashMap<String, String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				put("GBP", "5.45");
+			}
+		});
 		rateRepository.save(rate);
 	}
-	
+
 	@Test
 	public void createInstanceByReflection() {
 		assertNotNull(keyStoreExchangeApi);
 		assertTrue(keyStoreExchangeApi instanceof KeyStoreExchangeApi);
 	}
-	
+
 	@Test
 	public void createOptionalInstance() {
 		assertNotNull(optionalCurrencyExchangeApi);
 		assertTrue(optionalCurrencyExchangeApi instanceof KeyStoreExchangeApi);
 	}
-	
+
 	@Test
 	public void calculateExchange() {
 		CurrencyRate currencyRate = new CurrencyRate();
 		currencyRate.setAmount("5.45");
 		currencyRate.setFromCurrency("EUR");
 		currencyRate.setToCurrency("GBP");
-		
-		ExchangeCurrency exchange = keyStoreExchangeApi.exchangeSingleCurrency(currencyRate);
+
+		ExchangeCurrency exchange = keyStoreExchangeApi
+				.exchangeSingleCurrency(currencyRate);
 		assertNotNull(exchange.getExchange());
 		assertTrue(exchange.getExchange().equals(0) == false);
 		assertTrue(Double.valueOf(exchange.getExchange()) > 0);
 	}
-	
+
 	@Test
 	public void calculateExchangeWithLocaleFormat() {
 		CurrencyRate currencyRate = new CurrencyRate();
@@ -78,10 +80,11 @@ public class KeyStoreExchangeApiIT {
 		currencyRate.setFromCurrency("EUR");
 		currencyRate.setToCurrency("GBP");
 		currencyRate.setLocale("en_US");
-		
-		ExchangeCurrency exchange = keyStoreExchangeApi.exchangeSingleCurrency(currencyRate);
+
+		ExchangeCurrency exchange = keyStoreExchangeApi
+				.exchangeSingleCurrency(currencyRate);
 		assertTrue(exchange.getExchange().startsWith("$"));
-		assertTrue(Double.valueOf(exchange.getExchange().replace("$","")) > 0);
+		assertTrue(Double.valueOf(exchange.getExchange().replace("$", "")) > 0);
 	}
 
 }
