ce0ea5f3b59873a86b59bcfb2eefc18e51e6f862
Georgi Lambov <georgi.lambov@gmail.com>
2017-01-19 00:23:11 +0200

    Using streams in exchange rate builder.

--- currency-converter/src/main/java/org/apilytic/currency/exchange/ExchangeRateBuilder.java
+++ currency-converter/src/main/java/org/apilytic/currency/exchange/ExchangeRateBuilder.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apilytic.currency.persistence.domain.CurrencyExchange;
 import org.apilytic.currency.persistence.repository.CurrencyExchangeRepository;
@@ -12,54 +13,50 @@ import org.springframework.stereotype.Service;
 
 /**
  * Build cross-reference exchange rates.
- * 
+ *
  * @author Georgi Lambov
- * 
  */
 @Service
 public class ExchangeRateBuilder {
 
-	@Autowired
-	private CurrencyExchangeRepository currencyExchangeRepo;
-	private Map<String, Set<CurrencyExchange>> rates;
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepo;
+    private Map<String, Set<CurrencyExchange>> rates;
 
-	private ExchangeRate exchangeRate;
+    private ExchangeRate exchangeRate;
 
-	/**
-	 * Creates cross reference rates for currencies from currency ISO codes
-	 * persisted entities.
-	 */
-	protected void buildRates() {
-		Set<CurrencyExchange> currencies = currencyExchangeRepo
-				.findAllCurencyExchanges();
+    /**
+     * Creates cross reference rates for currencies from currency ISO codes
+     * persisted entities.
+     */
+    protected void buildRates() {
+        Set<CurrencyExchange> currencies = currencyExchangeRepo
+                .findAllCurencyExchanges();
 
-		rates = new HashMap<String, Set<CurrencyExchange>>();
-		for (CurrencyExchange currency : currencies) {
-			Set<CurrencyExchange> providedCurrencies = new HashSet<CurrencyExchange>(
-					currencies);
+        rates = new HashMap<>();
+        for (CurrencyExchange currency : currencies) {
+            Set<CurrencyExchange> providedCurrencies = currencies.stream().collect(Collectors.toSet());
+            providedCurrencies.remove(currency);
 
-			providedCurrencies.remove(currency);
+            rates.put(currency.getTitle(), providedCurrencies);
+        }
 
-			rates.put(currency.getTitle(), providedCurrencies);
-		}
+        exchangeRate.setRates(rates);
+    }
 
-		exchangeRate.setRates(rates);
-	}
+    /**
+     * Director to call builder for rates.
+     */
+    public void constructExchageRate() {
+        exchangeRate = new ExchangeRate();
+        buildRates();
+    }
 
-	/**
-	 * Director to call builder for rates.
-	 */
-	public void constructExchageRate() {
-		exchangeRate = new ExchangeRate();
-		buildRates();
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public ExchangeRate getExchangeRate() {
-		return exchangeRate;
-	}
+    /**
+     * @return
+     */
+    public ExchangeRate getExchangeRate() {
+        return exchangeRate;
+    }
 
 }
