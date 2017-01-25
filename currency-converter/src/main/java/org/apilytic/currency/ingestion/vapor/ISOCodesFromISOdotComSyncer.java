1a6304cf5d85b0fec9e33c7bec1882871abb41d8
Georgi Lambov <georgi.lambov@gmail.com>
2017-01-25 23:39:14 +0200

    using stream api.

--- currency-converter/src/main/java/org/apilytic/currency/ingestion/vapor/ISOCodesFromISOdotComSyncer.java
+++ currency-converter/src/main/java/org/apilytic/currency/ingestion/vapor/ISOCodesFromISOdotComSyncer.java
@@ -2,6 +2,7 @@ package org.apilytic.currency.ingestion.vapor;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apilytic.currency.ingestion.vapor.model.CurrencyCode;
 import org.apilytic.currency.ingestion.vapor.model.ISO4217Bean;
@@ -12,38 +13,36 @@ import org.springframework.stereotype.Service;
 
 /**
  * Sync ISO codes with database.
- * 
+ *
  * @author Georgi Lambov
- * 
  */
 @Service
 public class ISOCodesFromISOdotComSyncer implements VaporIngestion {
 
-	@Autowired
-	private FetchISOCodes fetchISOCodes;
+    @Autowired
+    private FetchISOCodes fetchISOCodes;
 
-	@Autowired
-	private CurrencyExchangeRepository currencyExchangeRepo;
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepo;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apilytc.currency.ingestion.vapor.VaporIngestion#sync()
-	 */
-	@Override
-	public void sync() {
-		ISO4217Bean isoCode = fetchISOCodes.fetch();
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apilytc.currency.ingestion.vapor.VaporIngestion#sync()
+     */
+    @Override
+    public void sync() {
+        ISO4217Bean isoCode = fetchISOCodes.fetch();
 
-		Set<org.apilytic.currency.ingestion.vapor.model.CurrencyCode> currencyCodes = isoCode
-				.getCurrencyTable().getCurrencyCodes();
+        Set<org.apilytic.currency.ingestion.vapor.model.CurrencyCode> currencyCodes = isoCode
+                .getCurrencyTable().getCurrencyCodes();
 
-		Set<CurrencyExchange> entities = new HashSet<CurrencyExchange>();
-		for (CurrencyCode currencyCode : currencyCodes) {
-			CurrencyExchange entity = new CurrencyExchange();
-			entity.setTitle(currencyCode.getIsoCode());
-			entities.add(entity);
-		}
+        Set<CurrencyExchange> entities = currencyCodes.stream().map(o -> {
+            CurrencyExchange entity = new CurrencyExchange();
+            entity.setTitle(o.getIsoCode());
+            return entity;
+        }).collect(Collectors.toSet());
 
-		currencyExchangeRepo.save(entities);
-	}
+        currencyExchangeRepo.save(entities);
+    }
 }
