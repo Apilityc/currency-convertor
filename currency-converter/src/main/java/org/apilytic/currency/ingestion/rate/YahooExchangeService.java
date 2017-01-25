5d16820ec2a64e8d94d0499d839f4a7003384bb8
Georgi Lambov <georgi.lambov@gmail.com>
2017-01-25 23:20:32 +0200

    using stream api.

--- currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/YahooExchangeService.java
+++ currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/YahooExchangeService.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.apilytic.currency.ingestion.rate.provider.ExchangeRate;
 import org.apilytic.currency.ingestion.rate.provider.YahooFinanceManager;
@@ -20,62 +21,59 @@ import org.springframework.stereotype.Service;
 
 /**
  * Sync currencies to database.
- * 
+ *
  * @author Georgi Lambov
- * 
  */
 @Service
 public class YahooExchangeService implements RateIngestion {
 
-	@Autowired
-	private YahooFinanceManager yahooFinanaceManager;
+    @Autowired
+    private YahooFinanceManager yahooFinanaceManager;
 
-	@Autowired
-	private YahooQueryRateBuilder queryRateBuilder;
+    @Autowired
+    private YahooQueryRateBuilder queryRateBuilder;
 
-	@Autowired
-	private RateRepository rateRepo;
+    @Autowired
+    private RateRepository rateRepo;
 
-	@Autowired
-	private YahooQueryRateParser rateParser;
+    @Autowired
+    private YahooQueryRateParser rateParser;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apilytic.currency.ingestion.query.RateIngestion#sync()
-	 */
-	@Override
-	public void sync() throws InterruptedException {
-		rateParser.setQueryRate(queryRateBuilder.createQueryRate());
-		Set<String> rateQueryChunks = rateParser.splitInChunks();
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apilytic.currency.ingestion.query.RateIngestion#sync()
+     */
+    @Override
+    public void sync() throws InterruptedException {
+        rateParser.setQueryRate(queryRateBuilder.createQueryRate());
+        Set<String> rateQueryChunks = rateParser.splitInChunks();
 
-		ExecutorService threadExecutor = Executors.newFixedThreadPool(5);
+        ExecutorService threadExecutor = Executors.newFixedThreadPool(5);
 
-		List<Rate> rates = new ArrayList<Rate>();
-		for (final String rateQuery : rateQueryChunks) {
-			threadExecutor.execute(() -> {
+        List<Rate> rates = new ArrayList<Rate>();
+        for (final String rateQuery : rateQueryChunks) {
+            threadExecutor.execute(() -> {
                 yahooFinanaceManager.setExchangeQuery(rateQuery);
                 final List<? extends ExchangeRate> providedRates = yahooFinanaceManager
                         .provideRate();
 
-                for (ExchangeRate exchangeRate : providedRates) {
+                Map<String, String> values = providedRates.stream()
+                        .collect(Collectors.toMap(o -> o.toCurrency(), o -> o.rate()));
 
-                    Map<String, String> values = new HashMap<String, String>();
-                    values.put(exchangeRate.toCurrency(),
-                            exchangeRate.rate());
-
+                providedRates.stream().map(o -> {
                     Rate r = new Rate();
-                    r.setKey(Rate.key(exchangeRate.fromCurrency()));
+                    r.setKey(Rate.key(o.fromCurrency()));
                     r.setValue(values);
-                    rates.add(r);
-                }
+                    return r;
+                }).collect(Collectors.toList()).addAll(rates);
             });
 
-		}
+        }
 
-		threadExecutor.shutdown();
-		threadExecutor.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
-		
-		rateRepo.save(rates);
-	}
+        threadExecutor.shutdown();
+        threadExecutor.awaitTermination(Integer.MAX_VALUE, TimeUnit.SECONDS);
+
+        rateRepo.save(rates);
+    }
 }
