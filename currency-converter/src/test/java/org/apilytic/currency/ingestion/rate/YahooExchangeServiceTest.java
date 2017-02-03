5d10f5020050790b80b1e50864838f8d6f51d088
gogoluxecs <gogo_luxecs@yahoo.com>
2017-02-03 22:39:53 +0200

    using stream api.

--- currency-converter/src/test/java/org/apilytic/currency/ingestion/rate/YahooExchangeServiceTest.java
+++ currency-converter/src/test/java/org/apilytic/currency/ingestion/rate/YahooExchangeServiceTest.java
@@ -1,20 +1,6 @@
 package org.apilytic.currency.ingestion.rate;
 
-import static org.apilytic.currency.ingestion.rate.provider.YahooQueryRateBuilder.queryRatePattern;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.apilytic.currency.ingestion.rate.provider.ExchangeRate;
-import org.apilytic.currency.ingestion.rate.provider.YahooCSVBean;
-import org.apilytic.currency.ingestion.rate.provider.YahooFinanceManager;
-import org.apilytic.currency.ingestion.rate.provider.YahooQueryRateBuilder;
-import org.apilytic.currency.ingestion.rate.provider.YahooQueryRateParser;
+import org.apilytic.currency.ingestion.rate.provider.*;
 import org.apilytic.currency.persistence.repository.RateRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,81 +8,87 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.apilytic.currency.ingestion.rate.provider.YahooQueryRateBuilder.queryRatePattern;
+import static org.mockito.Mockito.*;
+
 /**
- * 
  * @author Georgi Lambov
- * 
  */
 public class YahooExchangeServiceTest {
 
-	private RateIngestion rateIngestion;
-	@Mock
-	private YahooFinanceManager yahooFinanaceManager;
-	@Mock
-	private YahooQueryRateBuilder queryRateBuilder;
-	@Mock
-	private RateRepository rateRepo;
-	@Mock
-	private YahooQueryRateParser rateParser;
+    private RateIngestion rateIngestion;
+    @Mock
+    private YahooFinanceManager yahooFinanaceManager;
+    @Mock
+    private YahooQueryRateBuilder queryRateBuilder;
+    @Mock
+    private RateRepository rateRepo;
+    @Mock
+    private YahooQueryRateParser rateParser;
 
-	@Before
-	public void init() {
-		MockitoAnnotations.initMocks(this);
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
 
-		rateIngestion = new YahooExchangeService();
+        rateIngestion = new YahooExchangeService();
 
-		ReflectionTestUtils.setField(rateIngestion, "yahooFinanaceManager",
-				yahooFinanaceManager);
+        ReflectionTestUtils.setField(rateIngestion, "yahooFinanaceManager",
+                yahooFinanaceManager);
 
-		ReflectionTestUtils.setField(rateIngestion, "queryRateBuilder",
-				queryRateBuilder);
-		ReflectionTestUtils.setField(rateIngestion, "rateRepo", rateRepo);
-		ReflectionTestUtils.setField(rateIngestion, "rateParser", rateParser);
+        ReflectionTestUtils.setField(rateIngestion, "queryRateBuilder",
+                queryRateBuilder);
+        ReflectionTestUtils.setField(rateIngestion, "rateRepo", rateRepo);
+        ReflectionTestUtils.setField(rateIngestion, "rateParser", rateParser);
 
-	}
+    }
 
-	@Test
-	public void provideRate() throws InterruptedException {
+    @Test
+    public void provideRate() throws InterruptedException {
 
-		when(rateParser.splitInChunks())
-				.thenReturn(dataProviderForRateParser());
+        when(rateParser.splitInChunks())
+                .thenReturn(dataProviderForRateParser());
 
-		doReturn(dataProviderForRates()).when(yahooFinanaceManager)
-				.provideRate();
+        doReturn(dataProviderForRates()).when(yahooFinanaceManager)
+                .provideRate();
 
-		rateIngestion.sync();
+        rateIngestion.sync();
 
-		verify(yahooFinanaceManager).provideRate();
-		verify(queryRateBuilder).createQueryRate();
-		verify(rateParser).splitInChunks();
-	}
+        verify(yahooFinanaceManager).provideRate();
+        verify(queryRateBuilder).createQueryRate();
+        verify(rateParser).splitInChunks();
+    }
 
-	private List<? extends ExchangeRate> dataProviderForRates() {
+    private List<? extends ExchangeRate> dataProviderForRates() {
 
-		YahooCSVBean usdGbp = new YahooCSVBean();
-		usdGbp.setCurrency(String.format(
-				YahooQueryRateBuilder.queryRatePattern, "USD", "GBP"));
-		usdGbp.setRate("12.00");
+        YahooCSVBean usdGbp = new YahooCSVBean();
+        usdGbp.setCurrency(String.format(
+                YahooQueryRateBuilder.queryRatePattern, "USD", "GBP"));
+        usdGbp.setRate("12.00");
 
-		YahooCSVBean usdEur = new YahooCSVBean();
-		usdEur.setCurrency(String.format(
-				YahooQueryRateBuilder.queryRatePattern, "USD", "EUR"));
-		usdEur.setRate("1.75");
+        YahooCSVBean usdEur = new YahooCSVBean();
+        usdEur.setCurrency(String.format(
+                YahooQueryRateBuilder.queryRatePattern, "USD", "EUR"));
+        usdEur.setRate("1.75");
 
-		return Arrays.asList(usdGbp, usdEur);
-	}
+        return Arrays.asList(usdGbp, usdEur);
+    }
 
-	private Set<String> dataProviderForRateParser() {
-		String[] currencies = { "GBP", "EUR", "JPY" };
+    private Set<String> dataProviderForRateParser() {
+        String[] currencies = {"GBP", "EUR", "JPY"};
 
-		String queryRate = "";
-		for (String currency : currencies) {
-			queryRate += String.format(queryRatePattern, "USD", currency);
-		}
+        String queryRate = Arrays.stream(currencies)
+                .map(s -> String.format(queryRatePattern, "USD", s))
+                .collect(Collectors.joining());
 
-		Set<String> rates = new LinkedHashSet<String>();
-		rates.add(queryRate);
+        Set<String> rates = new LinkedHashSet<>();
+        rates.add(queryRate);
 
-		return rates;
-	}
+        return rates;
+    }
 }
