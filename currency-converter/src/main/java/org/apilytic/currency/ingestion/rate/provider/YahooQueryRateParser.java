41762d0f478faa7cc8ede0ba5e6ffdfd70a4f2f8
gogoluxecs <gogo_luxecs@yahoo.com>
2017-02-03 23:04:10 +0200

    re-format.

--- currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/provider/YahooQueryRateParser.java
+++ currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/provider/YahooQueryRateParser.java
@@ -1,103 +1,103 @@
 package org.apilytic.currency.ingestion.rate.provider;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * Parse in chunks Yahoo query query.
- * 
+ *
  * @author Georgi Lambov
- * 
  */
 @RooJavaBean
 @Service
 public class YahooQueryRateParser {
 
-	public static final int DEFAULT_STEP = 200;
-	public static final int ENTRY_LENGTH = String.format(
-			YahooQueryRateBuilder.queryRatePattern, "USD", "GBP").length();
+    public static final int DEFAULT_STEP = 200;
+    public static final int ENTRY_LENGTH = String.format(
+            YahooQueryRateBuilder.queryRatePattern, "USD", "GBP").length();
 
-	private int step = DEFAULT_STEP;
+    private int step = DEFAULT_STEP;
 
-	private StringBuilder queryRate;
+    private StringBuilder queryRate;
 
-	/**
-	 * Splits in query query in chunks.
-	 * 
-	 * @return
-	 */
-	public Set<String> splitInChunks() {
+    /**
+     * Splits in query query in chunks.
+     *
+     * @return
+     */
+    public Set<String> splitInChunks() {
 
-		if (StringUtils.isBlank(queryRate)) {
-			throw new IllegalArgumentException("queryRate can't be empty");
-		}
+        if (StringUtils.isBlank(queryRate)) {
+            throw new IllegalArgumentException("queryRate can't be empty");
+        }
 
-		Set<String> rateChunks = new LinkedHashSet<String>();
-		StringBuilder rates = new StringBuilder();
-		
-		// used in NEXT CHUNK string last position of each chunk
-		int lastChunkPosition = 0;
-		for (int i = 0; i < queryRate.length(); i = i + ENTRY_LENGTH) {
+        Set<String> rateChunks = new LinkedHashSet<String>();
+        StringBuilder rates = new StringBuilder();
 
-			// FIRST CHUNK
-			if (i == 0) {
-				
-				//TODO Use one for statement.
-				for (int stepIteration = 0; stepIteration < step; stepIteration++) {
-					//assign only first chunk with size of the ``step``
-					int endPos = (stepIteration * ENTRY_LENGTH) + ENTRY_LENGTH;
-					int startPos = stepIteration * ENTRY_LENGTH;
+        // used in NEXT CHUNK string last position of each chunk
+        int lastChunkPosition = 0;
+        for (int i = 0; i < queryRate.length(); i = i + ENTRY_LENGTH) {
 
-					if (endPos > queryRate.length()) {
-						break;
-					}
-					rates.append(queryRate.substring(startPos, endPos));
-				}
-				rateChunks.add(rates.toString());
-			}
+            // FIRST CHUNK
+            if (i == 0) {
 
-			// NEXT CHUNK
-			if (i > step * ENTRY_LENGTH) {
-				// clear rates query
-				rates = new StringBuilder();
-				for (int stepIteration = 0; stepIteration < step; stepIteration++) {
-					// stepSection valid value executed once.
-					int stepSection = i + (ENTRY_LENGTH * stepIteration);
-					// first iteration after initial load doesn't have chunk
-					// last position
-					if (lastChunkPosition > 0) {
-						stepSection = lastChunkPosition
-								+ (ENTRY_LENGTH * (stepIteration + 1));
-					}
+                //TODO Use one for statement.
+                for (int stepIteration = 0; stepIteration < step; stepIteration++) {
+                    //assign only first chunk with size of the ``step``
+                    int endPos = (stepIteration * ENTRY_LENGTH) + ENTRY_LENGTH;
+                    int startPos = stepIteration * ENTRY_LENGTH;
 
-					int startPos = stepSection - ENTRY_LENGTH;
-					int endPos = stepSection;
+                    if (endPos > queryRate.length()) {
+                        break;
+                    }
+                    rates.append(queryRate.substring(startPos, endPos));
+                }
+                rateChunks.add(rates.toString());
+            }
 
-					if (endPos > queryRate.length()) {
-						break;
-					}
+            // NEXT CHUNK
+            if (i > step * ENTRY_LENGTH) {
+                // clear rates query
+                rates = new StringBuilder();
+                for (int stepIteration = 0; stepIteration < step; stepIteration++) {
+                    // stepSection valid value executed once.
+                    int stepSection = i + (ENTRY_LENGTH * stepIteration);
+                    // first iteration after initial load doesn't have chunk
+                    // last position
+                    if (lastChunkPosition > 0) {
+                        stepSection = lastChunkPosition
+                                + (ENTRY_LENGTH * (stepIteration + 1));
+                    }
 
-					rates.append(queryRate.substring(startPos, endPos));
+                    int startPos = stepSection - ENTRY_LENGTH;
+                    int endPos = stepSection;
 
-					// retrieve last position of the read string
-					if (stepIteration + 1 == step) {
-						lastChunkPosition = endPos;
-					}
-				}
+                    if (endPos > queryRate.length()) {
+                        break;
+                    }
 
-				if (rates.length() == 0) {
-					break;
-				}
+                    rates.append(queryRate.substring(startPos, endPos));
 
-				rateChunks.add(rates.toString());
-			}
-		}
-		rates = new StringBuilder();;
+                    // retrieve last position of the read string
+                    if (stepIteration + 1 == step) {
+                        lastChunkPosition = endPos;
+                    }
+                }
 
-		return rateChunks;
-	}
+                if (rates.length() == 0) {
+                    break;
+                }
+
+                rateChunks.add(rates.toString());
+            }
+        }
+        rates = new StringBuilder();
+        ;
+
+        return rateChunks;
+    }
 }
