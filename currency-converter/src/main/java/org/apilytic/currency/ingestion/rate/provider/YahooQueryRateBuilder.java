72bc8fea85ef56afafea87911f401131ae68b348
gogoluxecs <gogo_luxecs@yahoo.com>
2017-02-03 23:00:32 +0200

    using stream api.

--- currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/provider/YahooQueryRateBuilder.java
+++ currency-converter/src/main/java/org/apilytic/currency/ingestion/rate/provider/YahooQueryRateBuilder.java
@@ -1,44 +1,46 @@
 package org.apilytic.currency.ingestion.rate.provider;
 
-import java.util.Map;
-import java.util.Set;
-
 import org.apilytic.currency.exchange.ExchangeRateBuilder;
 import org.apilytic.currency.persistence.domain.CurrencyExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * Create yahoo query query from exchange rates.
- * 
+ *
  * @author Georgi Lambov
  */
 @Service
 public class YahooQueryRateBuilder {
 
-	@Autowired
-	private ExchangeRateBuilder exchangeRateBuilder;
+    @Autowired
+    private ExchangeRateBuilder exchangeRateBuilder;
 
-	public static final String queryRatePattern = "&s=%s%s=X";
+    public static final String queryRatePattern = "&s=%s%s=X";
 
-	/**
-	 * Builds yahoo query query builder
-	 * 
-	 * @return
-	 */
-	public StringBuilder createQueryRate() {
-		exchangeRateBuilder.constructExchageRate();
-		Map<String, Set<CurrencyExchange>> rates = exchangeRateBuilder
-				.getExchangeRate().getRates();
+    /**
+     * Builds yahoo query query builder
+     *
+     * @return
+     */
+    public StringBuilder createQueryRate() {
+        exchangeRateBuilder.constructExchageRate();
+        Map<String, Set<CurrencyExchange>> rates = exchangeRateBuilder
+                .getExchangeRate().getRates();
 
-		StringBuilder sb = new StringBuilder();
-		for (String fromCurrency : rates.keySet()) {
-			for (CurrencyExchange toCurrency : rates.get(fromCurrency)) {
-				sb.append(String.format(queryRatePattern, fromCurrency,
-						toCurrency.getTitle()));
-			}
-		}
+        StringBuilder sb = new StringBuilder();
 
-		return sb;
-	}
+        String queryRate = rates.keySet().stream().map(fromCurrency -> {
+            return rates.get(fromCurrency).stream().map(s -> String.format(queryRatePattern, fromCurrency, s.getTitle
+                    ()))
+                    .collect(Collectors.joining());
+        }).collect(Collectors.joining());
+
+        sb.append(queryRate);
+        return sb;
+    }
 }
