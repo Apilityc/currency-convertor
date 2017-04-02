355b405bcd21994ca7f99ba8cef56f69a2af9fb4
gogoluxecs <gogo_luxecs@yahoo.com>
2017-04-02 21:28:49 +0300

    Removed current structure.

--- currency-converter/src/main/java/org/apilytic/currency/persistence/ApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.apilytic.currency.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories(repositoryImplementationPostfix = "Service")
-public class ApplicationConfiguration {
-}
--- currency-converter/src/main/java/org/apilytic/currency/persistence/repository/RateTimeTrackRepository.java
+++
@@ -1,9 +0,0 @@
-package org.apilytic.currency.persistence.repository;
-
-import org.apilytic.currency.persistence.domain.RateTimeTrack;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RateTimeTrackRepository extends
-		CrudRepository<RateTimeTrack, String> {
-
-}
