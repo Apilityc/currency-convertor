560adbc01cd1ad0a9774de2da8c794c965c2714c
gogoluxecs <gogo_luxecs@yahoo.com>
2017-04-04 00:12:07 +0300

    Spring aop, spring-data-redis integration.

--- src/main/java/org/apilytic/currency/HelloWorld.java
+++ src/main/java/org/apilytic/currency/HelloWorld.java
@@ -1,6 +1,8 @@
 package org.apilytic.currency;
 
 import io.reactivex.Flowable;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by g on 4/2/17.
@@ -12,6 +14,9 @@ public class HelloWorld {
 	}
 
 	public static void main(String[] args) {
-		Flowable.just("Hello world").subscribe(System.out::println);
+		ApplicationContext context = new ClassPathXmlApplicationContext
+				("classpath*:/META-INF/spring/applicationContext*.xml");
+		Service s = context.getBean(Service.class);
+		Flowable.just(s.output("Hello World!")).subscribe(System.out::println);
 	}
 }
