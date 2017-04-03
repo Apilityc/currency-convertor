0cd32a054f7882ef8753593b4fe540e8a5805dcf
gogoluxecs <gogo_luxecs@yahoo.com>
2017-04-03 21:53:31 +0300

    Support for rxJava.

--- src/main/java/org/apilytic/currency/HelloWorld.java
+++ src/main/java/org/apilytic/currency/HelloWorld.java
@@ -1,5 +1,7 @@
 package org.apilytic.currency;
 
+import io.reactivex.Flowable;
+
 /**
  * Created by g on 4/2/17.
  */
@@ -7,5 +9,9 @@ public class HelloWorld {
 
 	public int sum(int a, int b) {
 		return a + b;
+	}
+
+	public static void main(String[] args) {
+		Flowable.just("Hello world").subscribe(System.out::println);
 	}
 }
