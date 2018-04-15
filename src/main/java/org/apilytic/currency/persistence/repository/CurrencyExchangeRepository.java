613f42f44561cd839d04615c8aba73be1e450412
georgi <gogo_luxecs@yahoo.com>
2018-04-15 23:25:55 +0300

    Remove overrided interface methods.

--- src/main/java/org/apilytic/currency/persistence/repository/CurrencyExchangeRepository.java
+++ src/main/java/org/apilytic/currency/persistence/repository/CurrencyExchangeRepository.java
@@ -3,9 +3,5 @@ package org.apilytic.currency.persistence.repository;
 import org.apilytic.currency.persistence.domain.CurrencyExchange;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Set;
-
 public interface CurrencyExchangeRepository extends CrudRepository<CurrencyExchange, String> {
-
-	Set<CurrencyExchange> findAllCurrencyExchanges();
 }
--- src/main/java/org/apilytic/currency/persistence/repository/ExchangeRepository.java
+++ src/main/java/org/apilytic/currency/persistence/repository/ExchangeRepository.java
@@ -3,8 +3,5 @@ package org.apilytic.currency.persistence.repository;
 import org.apilytic.currency.persistence.domain.Exchange;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Set;
-
 public interface ExchangeRepository extends CrudRepository<Exchange, String> {
-	Set<Exchange> findAllExchanges();
 }
