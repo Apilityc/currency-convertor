2b1c9824afffc8cdd1be57876361ca3c2502a4ca
georgi <gogo_luxecs@yahoo.com>
2018-05-28 23:39:05 +0300

    Basic exchange class.

---
+++ src/main/java/org/apilytic/currency/exchange/Exchanger.java
@@ -0,0 +1,10 @@
+package org.apilytic.currency.exchange;
+
+import org.apilytic.currency.persistence.domain.CurrencyPair;
+
+public class Exchanger implements RateExchnage {
+	@Override
+	public String exchange(CurrencyPair pair) {
+		return null;
+	}
+}
