caaf217f07581110c72a3501c12238564fe9cdd4
georgi <gogo_luxecs@yahoo.com>
2018-05-28 23:41:41 +0300

    Basic exchanger coverage.

---
+++ src/test/java/org/apilytic/currency/exchange/ExchangerTest.java
@@ -0,0 +1,18 @@
+package org.apilytic.currency.exchange;
+
+import org.apilytic.currency.persistence.domain.CurrencyPair;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertNull;
+
+public class ExchangerTest {
+
+	@Test
+	public void exchanger() {
+
+		CurrencyPair pair = Mockito.mock(CurrencyPair.class);
+		Exchanger exchanger = new Exchanger();
+		assertNull(exchanger.exchange(pair));
+	}
+}
