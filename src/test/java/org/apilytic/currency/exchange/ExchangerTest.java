224f92b6fa508ca57a58d322ab1a1dc8d7ab9e83
georgi <gogo_luxecs@yahoo.com>
2018-05-30 01:01:24 +0300

    Remove iniline init.

--- src/test/java/org/apilytic/currency/exchange/ExchangerTest.java
+++ src/test/java/org/apilytic/currency/exchange/ExchangerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -28,11 +29,12 @@ public class ExchangerTest {
 		when(pair.from()).thenReturn("USD");
 		when(exchangeRepository.findOne("USD")).thenReturn(exchange);
 
-		when(exchange.getRates()).thenReturn(new HashMap() {{
-												 put("USD", "1.9");
-												 put("EUR", "1.8");
-											 }}
-		);
+		Map rates = new HashMap() {{
+			put("USD", "1.9");
+			put("EUR", "1.8");
+		}};
+
+		when(exchange.getRates()).thenReturn(rates);
 
 		System.out.println(exchanger.exchange(pair));
 		assertEquals("1.8", exchanger.exchange(pair));
