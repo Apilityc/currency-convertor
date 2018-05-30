ff7358c922d6d2cba462933ad7d7affde71f0b82
georgi <gogo_luxecs@yahoo.com>
2018-05-31 01:46:43 +0300

    Cli for exchange rate.

--- src/main/java/org/apilytic/currency/HelloWorld.java
+++ src/main/java/org/apilytic/currency/HelloWorld.java
@@ -1,8 +1,10 @@
 package org.apilytic.currency;
 
 import io.reactivex.Flowable;
+import org.apilytic.currency.exchange.Exchanger;
 import org.apilytic.currency.ingestion.code.IsoCodesSyncher;
 import org.apilytic.currency.ingestion.rate.RateSyncher;
+import org.apilytic.currency.persistence.domain.CurrencyPair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -26,6 +28,7 @@ public class HelloWorld {
 
 		String[] command = args[0].split(":");
 
+		// format is command:option e.g. ingestion:currency
 		if (command[0].equals("ingestion")) {
 			if (command[1].equals("currency")) {
 				IsoCodesSyncher syncher = context.getBean(IsoCodesSyncher.class);
@@ -34,6 +37,15 @@ public class HelloWorld {
 				RateSyncher syncher = context.getBean(RateSyncher.class);
 				syncher.sync();
 			}
+		} else if (command[0].equals("exchange")) {
+			CurrencyPair pair = context.getBean(CurrencyPair.class);
+
+			pair.setFrom(command[1].substring(0, 3));
+			pair.setTo(command[1].substring(3, 6));
+
+			Exchanger exchanger = context.getBean(Exchanger.class);
+
+			Flowable.just(exchanger.exchange(pair)).subscribe(System.out::println);
 		}
 
 	}
