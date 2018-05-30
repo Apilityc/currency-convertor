24314c405ad557c92beca0069a8899f697af418e
georgi <gogo_luxecs@yahoo.com>
2018-05-31 01:29:31 +0300

    @Primary impl.

--- src/main/java/org/apilytic/currency/exchange/RateExchange.java
+++ src/main/java/org/apilytic/currency/exchange/RateExchange.java
@@ -1,10 +1,12 @@
 package org.apilytic.currency.exchange;
 
 import org.apilytic.currency.persistence.domain.CurrencyPair;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Internal API for exchange rate based on @see {@link CurrencyPair}
  */
+@Primary
 public interface RateExchange {
 
 	/**
