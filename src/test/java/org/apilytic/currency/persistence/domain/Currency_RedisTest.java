9d5871bb2f7b0ae42bdc46b2efd52c4b9fc873ed
georgi <gogo_luxecs@yahoo.com>
2018-06-07 00:08:48 +0300

    Coverage @RedisHash value.

---
+++ src/test/java/org/apilytic/currency/persistence/domain/Currency_RedisTest.java
@@ -0,0 +1,21 @@
+package org.apilytic.currency.persistence.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+import org.springframework.data.redis.core.RedisHash;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Currency_RedisTest {
+
+	@Test
+	public void valueRedisHashAnnotation() {
+		Currency currency = new Currency();
+		AspectJProxyFactory factory = new AspectJProxyFactory(currency);
+		Currency_Redis aspect = new Currency_Redis();
+		factory.addAspect(aspect);
+		Object proxy = factory.getProxy();
+
+		assertEquals("currency", proxy.getClass().getAnnotation(RedisHash.class).value());
+	}
+}
