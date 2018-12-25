8c710e602c3e762a842170385d94c1589ddfbeb4
georgi <gogo_luxecs@yahoo.com>
2018-12-25 03:51:00 +0200

    Remove unused code.

--- src/test/java/org/apilytic/currency/ingestion/rate/RateSyncherTest.java
+++ src/test/java/org/apilytic/currency/ingestion/rate/RateSyncherTest.java
@@ -3,10 +3,8 @@ package org.apilytic.currency.ingestion.rate;
 import org.apilytic.currency.parser.RateParser;
 import org.apilytic.currency.persistence.domain.Currency;
 import org.apilytic.currency.persistence.domain.CurrencyPair;
-import org.apilytic.currency.persistence.domain.Exchange;
 import org.apilytic.currency.persistence.domain.YahooChart;
 import org.apilytic.currency.persistence.repository.CurrencyRepository;
-import org.apilytic.currency.persistence.repository.ExchangeRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,17 +16,12 @@ import static org.mockito.Mockito.*;
 
 public class RateSyncherTest {
 
-
-	@Mock
-	private ExchangeRepository exchangeRepo;
 	@Mock
 	private CurrencyRepository currencyRepo;
 	@Mock
 	private RateFetch rateFetcher;
 	@Mock
 	private RateParser rateParser;
-	@Mock
-	private Exchange exchange;
 	@Mock
 	private Currency currency;
 	@Mock
