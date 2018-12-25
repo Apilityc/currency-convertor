f3048db892099d5eb63f0b4b519c62a2742914c9
georgi <gogo_luxecs@yahoo.com>
2018-12-25 04:09:11 +0200

    restore exchange repo and domain.

--- src/test/java/org/apilytic/currency/ingestion/rate/RateSyncherTest.java
+++ src/test/java/org/apilytic/currency/ingestion/rate/RateSyncherTest.java
@@ -3,8 +3,10 @@ package org.apilytic.currency.ingestion.rate;
 import org.apilytic.currency.parser.RateParser;
 import org.apilytic.currency.persistence.domain.Currency;
 import org.apilytic.currency.persistence.domain.CurrencyPair;
+import org.apilytic.currency.persistence.domain.Exchange;
 import org.apilytic.currency.persistence.domain.YahooChart;
 import org.apilytic.currency.persistence.repository.CurrencyRepository;
+import org.apilytic.currency.persistence.repository.ExchangeRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,12 +18,17 @@ import static org.mockito.Mockito.*;
 
 public class RateSyncherTest {
 
+
+	@Mock
+	private ExchangeRepository exchangeRepo;
 	@Mock
 	private CurrencyRepository currencyRepo;
 	@Mock
 	private RateFetch rateFetcher;
 	@Mock
 	private RateParser rateParser;
+	@Mock
+	private Exchange exchange;
 	@Mock
 	private Currency currency;
 	@Mock
